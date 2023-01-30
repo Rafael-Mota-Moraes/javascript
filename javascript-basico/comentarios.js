be7eb054b6b479b3705760bbe10744c8f168c798
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-30 11:29:31 -0300

    Navegador vs Node (HTML + JavaScript)

---
+++ javascript-basico/comentarios.js
@@ -0,0 +1,9 @@
+// Escrevo um comentário aqui...
+console.log("Olá mundo 1"); // O comentário não precisa ser no início da linha..
+// console.log("Olá mundo 2");
+
+/**
+ * Comentário de
+ * Várias
+ * linhas...
+ */
