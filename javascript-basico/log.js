13f399bd5659c6a0e1e72d6f38972aaf1611a4b7
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-30 10:55:49 -0300

    console.log

---
+++ javascript-basico/log.js
@@ -0,0 +1,10 @@
+console.log('Metodo "log" dentro do objeto "console"');
+console.log("Aspas simples ou duplas");
+console.log(`
+  Quebro
+  a
+  Linha....
+`);
+
+console.log(1, 2, 3, 4, 5, 6); // Não preciso dea aspas!!!
+console.log(1.1);
