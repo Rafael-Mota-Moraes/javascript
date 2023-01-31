af0886ea3965ecbf30d5b7cf00cad5dccfedb88b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-31 10:41:05 -0300

    Exercício variáveis

---
+++ javascript-basico/exercicio_const_let.js
@@ -0,0 +1,10 @@
+const nome = "Rafael";
+const idade = 30;
+const peso = 80;
+const altura = 1.64;
+const imc = peso / (altura * altura);
+
+console.log(`
+  ${nome} tem ${idade} anos, pesa ${peso}Kg tem ${altura} de altura.
+  Seu IMC é: ${imc}
+`);
