57bc9a06e02865949288ebfa296c147bc686a119
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-31 10:17:44 -0300

    Variáveis com let

---
+++ javascript-basico/let.js
@@ -0,0 +1,13 @@
+let nome; // SÓ DECLAROU
+console.log(nome);
+
+nome = "Rafael"; // ATRIBUIU O VALOR
+
+console.log(nome, "nasceu em 2004");
+console.log(nome, "tem 18 anos");
+
+// let nome; gera erro!!
+
+// Não podemos criar variáveis com palavras reservadas
+// Ex: let if
+// Dê nomes claros as suas variáveis
