0c505c25ffa021b9ea4f2b65c499ce4e2c927394
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-31 11:08:11 -0300

    Tipos de dados primitivos

---
+++ javascript-basico/dados_primitivos.js
@@ -0,0 +1,9 @@
+const nome = "Rafael"; // String
+const numero = 10; // number
+const numero2 = 10.1; // number
+let indefinido; // undefined = Não aponta para nenhum local da memória
+let nulo = null; // Nulo
+
+// Booleanos
+const verdadeiro = true;
+const falso = false;
