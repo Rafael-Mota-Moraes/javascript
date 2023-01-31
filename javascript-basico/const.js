45cf0f08d915a2d2236b5a8c28bfdc7f28e3a9ea
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-01-31 10:38:07 -0300

    Const

---
+++ javascript-basico/const.js
@@ -0,0 +1,13 @@
+const nome = "Rafael";
+console.log(nome);
+
+// nome = "João"; VAI GERAR UM ERRO!
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado, resultadoDuplicado);
+console.log(typeof primeiroNumero);
