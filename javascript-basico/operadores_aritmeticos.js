97039948e56ef73b9124b951b9dd234fc86c21a8
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-01 09:58:18 -0300

    Operadores

---
+++ javascript-basico/operadores_aritmeticos.js
@@ -0,0 +1,21 @@
+// Aritméticos: + - / *
+// % Resto da divisão
+const n1 = 5;
+const n2 = 10;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(2 ** 10); // Potenciação
+console.log(n1 % n2);
+
+// Incremento e decremento
+
+let contador = 1;
+contador++;
+console.log(contador);
+contador--;
+console.log(contador);
+
+console.log();
