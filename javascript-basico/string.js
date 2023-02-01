bcc3024ab6c518c2a7afbd5c92b46e6994f5d5c5
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-01 10:22:55 -0300

    Métodos da string

---
+++ javascript-basico/string.js
@@ -0,0 +1,12 @@
+let string = 'Um "Texto"';
+
+console.log(string);
+console.log(string[0]); // Podemos acessar as letras pelo índice
+console.log(string.charAt(0));
+console.log(string.concat(" muito", " legal"));
+console.log(string.lastIndexOf("t"));
+console.log(string.match(/[a-zA-Z]/g));
+console.log(string.replace("Um", "Meu"));
+console.log(string.length);
+console.log(string.slice(3, 10));
+console.log(string.split(" "));
