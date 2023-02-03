4e47ebbe9068864164ab3f7134bc1d1ae596c4b8
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-03 10:50:20 -0300

    Avaliação de Curto-Circuito (Short-Circuit)

---
+++ logica-programacao/curto_circuito.js
@@ -0,0 +1,9 @@
+console.log("Rafael" && 1 && "Outra coisa");
+console.log("Rafael" && 0 && "Outra coisa");
+
+function falaOi() {
+  return "Oi";
+}
+
+const vaiExecutar = true;
+console.log(vaiExecutar && falaOi());
