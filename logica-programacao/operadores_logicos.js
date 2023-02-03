45804d4739094ca3f7933a424aa015d829cf4089
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-03 10:40:40 -0300

    Operadores Lógicos

---
+++ logica-programacao/operadores_logicos.js
@@ -0,0 +1,13 @@
+/**
+ * Operadores lógicos
+ * && -> AND -> E
+ * || -> OR -> OU
+ * ! -> NOT -> NÃO
+ */
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+console.log(!true);
+console.log(!false);
