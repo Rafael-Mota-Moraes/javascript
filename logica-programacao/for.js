8c8ada0e3550b7662c6ef55f02d660a8e5f0d6ac
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-05 11:36:15 -0300

    Laço for

---
+++ logica-programacao/for.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++) {
+  console.log(`I = ${i}`);
+}
+
+const nomes = ["Rafael", "Marcos", "Marcelo", "Julia"];
+for (let i = 0; i < nomes.length; i++) {
+  console.log(nomes[i]);
+}
