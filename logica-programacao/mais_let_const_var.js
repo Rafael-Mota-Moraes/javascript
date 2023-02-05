ff33da1afbcb83a8c7bc9f5ba2dbb10ab62b48c4
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-05 10:04:37 -0300

    Mais diferenças entre var, let e const

---
+++ logica-programacao/mais_let_const_var.js
@@ -0,0 +1,19 @@
+const verdadeira = true;
+
+let nome = "Rafael";
+// let nome = 'Rafael'
+
+var nome2 = "Rafael";
+var nome2 = "Rafael";
+
+if (verdadeira) {
+  let nome = "Marcio";
+  console.log("Ok");
+  console.log(nome, nome2);
+
+  if (verdadeira) {
+    var nome2 = "João";
+    console.log("Ok");
+    console.log(nome, nome2);
+  }
+}
