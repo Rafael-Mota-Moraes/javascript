a9c058161aca90fde02e20cc4781c2aea8e71a8f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-05 11:20:32 -0300

    Desestruturação Objetos

---
+++ logica-programacao/desestruturacao_objeto.js
@@ -0,0 +1,12 @@
+const pessoa = {
+  nome: "Rafael",
+  sobrenome: "Moreira",
+  idade: 18,
+  endereco: {
+    rua: "AV Brasil",
+    numero: 320
+  }
+};
+
+const { nome, sobrenome } = pessoa;
+console.log(nome, sobrenome);
