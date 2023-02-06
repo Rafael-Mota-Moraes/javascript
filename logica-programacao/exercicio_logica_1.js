b7f9af19b0af2d9f56d4e46dfceb8a952565794f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-06 18:27:08 -0300

    Exercicios de lógica

---
+++ logica-programacao/exercicio_logica_1.js
@@ -0,0 +1,5 @@
+function retornaMaiorNumero(n1, n2) {
+  return n1 > n2 ? "N1 MAIOR" : "N2 MAIOR";
+}
+
+console.log(retornaMaiorNumero(2, 1));
---
+++ logica-programacao/exercicio_logica_2.js
@@ -0,0 +1,9 @@
+function ePaisagem(largura, altura) {
+  if (largura > altura) {
+    console.log("Está no modo paisagem.");
+  } else {
+    console.log("Não está no modo paisagem.");
+  }
+}
+
+ePaisagem(1920, 1080);
