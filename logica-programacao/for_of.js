45cedabe0633805d77ea0529c91c8620170980cc
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-06 09:48:54 -0300

    for of

---
+++ logica-programacao/for_of.js
@@ -0,0 +1,11 @@
+const nome = "Rafael Moreira";
+
+for (let letra of nome) {
+  console.log(letra);
+}
+
+const nomes = ["Rafael", "Luiz", "Henrique"];
+
+for (let nome of nomes) {
+  console.log(nome);
+}
