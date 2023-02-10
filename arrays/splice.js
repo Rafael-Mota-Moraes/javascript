a6d8d3966d14584eeb6a6fb8fc5da36d6038fba5
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-10 16:11:36 -0300

    Método splice

---
+++ arrays/splice.js
@@ -0,0 +1,8 @@
+const nomes = ["Maria", "João", "Eduardo", "Gabriel", "Júlia"];
+// nomes.splice(índice, delete, el1, el2, el3)
+
+const removidos = nomes.splice(4, 1);
+
+console.log(nomes, removidos);
+
+console.log(Number.MAX_VALUE);
