18aba38c63e0a4c186d04aa5cc24d18fa04721aa
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-02-14 10:48:41 -0300

    Herança

---
+++ objetos/heranca.js
@@ -0,0 +1,23 @@
+function Produto(nome, preco) {
+  this.nome = nome;
+  this.preco = preco;
+}
+
+Produto.prototype.aumento = function (quantia) {
+  this.preco += quantia;
+};
+
+Produto.prototype.desconto = function (quantia) {
+  this.preco += quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+  Produto.call(this, nome, preco);
+  this.cor = cor;
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+
+const camiseta = new Camiseta("Regata", 15, "Preta");
+camiseta.aumento(10);
+console.log(camiseta);
